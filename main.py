c5c86fec6c2476671daab4332aaae8e816d45aaa
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-02-28 21:49:07 +0300

    done 2 api request - about coin and coin date

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from api_request_cbr import Api_coin
 
 app = FastAPI(
     title='Testing Server Espinosa'
@@ -8,6 +9,20 @@ app = FastAPI(
 # название функции будет отображено в описании документации. Подбирай название
 @app.get('/')
 def hello():
-    return "Hello Worldss"
+    return "Hello World"
 
 
+@app.get('/coin/{coin}')
+def get_coin_course(coin:str):
+    check_coin = Api_coin(coin, 'some_date')
+    result = check_coin.check_coin_today()
+
+    return result
+
+
+@app.get('/coin/{coin}/{date}')
+def get_coin_course_date(coin:str, date:str):
+    check_coin = Api_coin(coin, date)
+    result = check_coin.check_coin_some_date()
+
+    return result
